97b5ec724d0f0faedff764594c2f6894aed4cfe9
emacs45 <49686253+emacs45@users.noreply.github.com>
2023-04-18 14:33:31 +0200

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,11 +1,11 @@
 import random
 import re
-import hangman_asciiDarstellung
-import hangman_woerter
+import hangman_asciiDarstellung as ascii
+import hangman_woerter as words
 
 def print_header(word):
     """
-    Zeigt das Hangman-Logo, eine Willkommensnachricht und die Länge des ausgewählten Wortes an.
+    Zeigt das Hangman-Logo, eine Willkommensnachricht und die Länge des ausgewählten Wortes an
 
     Parameter:
     word (str): Das geheime Wort, das der Benutzer erraten muss.
@@ -13,15 +13,14 @@ def print_header(word):
     Rückgabe:
     None
     """
-    print(hangman_asciiDarstellung.logo)
+    print(ascii.logo)
     print("Welcome to Hangman!")
     print(f"I think of a word, which is {len(word)} characters long")
 
-def print_hangman_lifes(guessedLetter, chosenWord, lifes, alreadyguessed, secretWordList):
+def print_hangman(guessedLetter, chosenWord, lifes, alreadyguessed, secretWordList):
     """
-    Überprüft, ob Buchstabe im Wort enthalten ist, sonst Lebensabzug
     Überprüft, ob das Spiel gewonnen/ verloren wurde
-    Zeigt, welche Buchstaben bereits erraten sind
+    Zeigt, welche Buchstaben bereits (nicht) erraten sind
     Zeigt, wie viele Leben übrig sind
 
     Parameter:
@@ -32,54 +31,54 @@ def print_hangman_lifes(guessedLetter, chosenWord, lifes, alreadyguessed, secret
     secretWordList (list): mit "_" versehenes Wort 
 
     Rückgabe:
-    lifes
+    None
     """
     if "".join(secretWordList) == chosenWord:
+        print("**************************************")
         print("Congratulations! You won!!!")
         print("**************************************")
-
+    
     if guessedLetter not in chosenWord:
-        lifes -= 1
-        
+
         if lifes == 0:
             print("You Lost!")
             print("**************************************")      
-            print(hangman_asciiDarstellung.stufen[lifes])
+            print(ascii.stufen[lifes])
             print(f"The word was \"{chosenWord}\"")
         else:
-            print(hangman_asciiDarstellung.stufen[lifes])
+            print(ascii.stufen[lifes])
             print("**************************************")
-            print(f"Sorry, \"{guessedLetter}\" seems not right!")
+            print(f"Sorry, \"{guessedLetter}\" is not right!")
             print("**************************************")
 
+    print(f"Hangman: {' '.join(secretWordList)}")
+    print("**************************************")
     print(f"You already guessed: {alreadyguessed}") 
     print(f"Remaining lifes: {lifes}")
-        
-    return lifes
+    print(f"Remaining letters: {secretWordList.count('_')}")
+    print("**************************************")
 
 def get_input():
     """
     Erhält User-Eingabe mit RegEx
-    Erlaubte Zeichen: A-Z Incase-Sensitive + Umlaute + "-" + " "
+    Erlaubte Zeichen: A-Z case-insensitive + Umlaute + "-" + " "
 
     Parameter:
     None
 
     Rückgabe:
-    guess (str): Ein Wert zwischen A-Z
+    guess (str): Ein einzelner Character zwischen A-Z, a-z, äüö, - , " " 
     """
     match = False
 
     while not match:
 
-        guess = input("Please enter a letter: ")
-        pattern = r"^[a-zA-Z,\ä\ö\ü\ß\-\ ]{1}$"
+        guess = input("Enter a valid letter between [A-Z]: ")
+        pattern = r"^[a-zA-z,\ä\ö\ü\ß\-\ ]$"
         match = re.search(pattern, guess)
 
         if match:
             return guess
-        else:
-            print("Enter a valid letter between [A-Z]: ")
 
 def load_wordlist():
     """
@@ -93,12 +92,12 @@ def load_wordlist():
     wordList (list)
     """
     #wordList = ["Katze", "Hund", "Maus", "Hundun"]
-    wordList = hangman_woerter.hangman_woerter
+    wordList = words.hangman_woerter
     return wordList
 
 def get_secretwordlist(wordList):
     """
-    Chiffriert WordList mit _
+    Maskiert WordList mit _
 
     Parameter:
     wordList (list): Vom Programm gewähltes Wort
@@ -106,23 +105,42 @@ def get_secretwordlist(wordList):
     Rückgabe:
     secretwordlist (list)
     """
-    return ["_" for _ in range(len(wordList))]
+    return list(len(wordList) * "_")
 
-def hangman(chosenWord, guessedLetter, secretWordList, alreadyguessed):
+def hangman(chosenWord, guessedLetter, secretWordList, alreadyguessed, lifes):
+    """
+    Überprüft, ob Buchstabe bereits erraten 
+    -> Ja, Ausgabe bereits erraten
+    -> Nein, ist Buchstabe in Wort enthalten?
+        -> Ja, führe Ersetzung durch
+        -> Nein, Lebensabzug
+    Füge Buchstabe in Set ein
 
-    for i in range(len(chosenWord)):
-        if guessedLetter == chosenWord[i]:
-            secretWordList[i] = chosenWord[i]
-        
-        alreadyguessed.add(guessedLetter)
+    Parameter:
+    chosenWord (str): vom Computer ausgewähltes Wort
+    guessedLetter (str): vom User gewählter Buchstabe
+    secretWordList (list): mit "_" maskiertes Wort (Liste von Strings)
+    alreadyguessed (set): bereits erratene Buchstaben
+    lifes (int): Anzahl Leben
 
-    print("**************************************")
-    print(f"Hangman: {' '.join(secretWordList)}")
-    print(f"Remaining letters: {secretWordList.count('_')}")
-    print("**************************************")
-    #print(f"Debug: {alreadyguessed}")
+    Rückgabe:
+    wordList (list)
+    """
 
-    return secretWordList, alreadyguessed
+    if guessedLetter not in alreadyguessed:
+        if guessedLetter in chosenWord:
+            for i in range(len(chosenWord)):
+                if guessedLetter == chosenWord[i]:
+                    secretWordList[i] = chosenWord[i]
+        else:
+            lifes -= 1
+    else:
+        print("**************************************")
+        print(f"Ouups!! You already guessed: {guessedLetter}!")
+        print("**************************************")
+    alreadyguessed.add(guessedLetter)
+
+    return secretWordList, alreadyguessed, lifes
 
 if __name__ == "__main__":
     
@@ -134,10 +152,8 @@ if __name__ == "__main__":
 
     print_header(chosenWord)
     print(f"DEBUG: Das gewaehlte Wort ist {chosenWord}")
-
-    # while not ("".join(secretWord) in chosenWord) and lifes > 0:
     while "_" in secretWord and lifes > 0:
         
         guessedLetter = get_input().lower()
-        secretWord, alreadyguessed = hangman(chosenWord, guessedLetter, secretWord, alreadyguessed)
-        lifes = print_hangman_lifes(guessedLetter, chosenWord, lifes, alreadyguessed, secretWord)
+        secretWord, alreadyguessed, lifes = hangman(chosenWord, guessedLetter, secretWord, alreadyguessed, lifes)
+        print_hangman(guessedLetter, chosenWord, lifes, alreadyguessed, secretWord)
